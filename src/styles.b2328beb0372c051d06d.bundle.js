cff89f3e6b19b21b7121a305613d592071b9ec6a
SergeyDushkin <leirbythe@gmail.com>
2017-01-28 14:43:55 +0300

    update

--- src/styles.b2328beb0372c051d06d.bundle.js
+++ src/styles.b2328beb0372c051d06d.bundle.js
@@ -1,2 +1,2 @@
-webpackJsonp([1,3],{1220:function(n,o,c){n.exports=c(570)},570:function(n,o){}},[1220]);
+webpackJsonp([1,3],{1224:function(n,o,c){n.exports=c(578)},578:function(n,o){}},[1224]);
 //# sourceMappingURL=styles.b2328beb0372c051d06d.bundle.map
