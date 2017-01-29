f9af7d186e06c28e5ed733153706ef4e36cc34b5
SergeyDushkin <leirbythe@gmail.com>
2017-01-29 19:22:24 +0300

    @

---
+++ src/config.js
@@ -0,0 +1,9 @@
+var APP_SETTINGS = {
+    "APP_API_URI" : "/api/",
+    "APP_HUB_URI" : "http://docker-provided-apiServerBaseAddress:8080/hub",
+    "AuthenticationServer" : {
+        "url" : "/authorization/",
+        "client_id" : "public",
+        "client_secret" : "public"
+        }
+};
--- src/styles.b2328beb0372c051d06d.bundle.js
+++
@@ -1,2 +0,0 @@
-webpackJsonp([1,3],{1224:function(n,o,c){n.exports=c(578)},578:function(n,o){}},[1224]);
-//# sourceMappingURL=styles.b2328beb0372c051d06d.bundle.map
