992017793c5ea1df05a19f650b17d3950fc117ee
SergeyDushkin <leirbythe@gmail.com>
2017-02-21 09:21:02 +0300

    web

--- src/config.js
+++ src/config.js
@@ -2,6 +2,7 @@ var APP_SETTINGS = {
     "APP_API_URI" : "/api/",
     "APP_HUB_URI" : "http://docker-provided-apiServerBaseAddress:8080/hub",
     "APP_TICKET_SERVICE_API_URI" : "http://docker-provided-apiServerBaseAddress:10020/",
+    "APP_DRIVE_SERVICE_API_URI" : "http://docker-provided-apiServerBaseAddress:10030/",
     "AuthenticationServer" : {
         "url" : "/authorization/",
         "client_id" : "public",
